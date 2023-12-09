9cf4bb655b29d33aaf566bd0864c760b0f872c80
Adam <N00211418@iadt.ie>
2023-12-09 19:08:17 +0000

    created footer / nav

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer style={{ position: 'fixed', bottom: 0 }} className="footer footer-center p-4 bg-base-300 text-base-content">
+      <aside>
+        <div className="text-sm breadcrumbs">
+          <ul>
+            <li><a>Github</a></li>
+            <li><a>Linkedln</a></li>
+            {/* <li>Add Document</li> */}
+          </ul>
+        </div>
+      </aside>
+    </footer>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,38 @@
-import { Link } from 'react-router-dom'
+import { useNavigate, useLocation } from "react-router-dom"
 
 const Navbar = () => {
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const navigateHome = () => {
+    navigate('/')
+  }
+
+  const navigateProjects = () => {
+    navigate('/projects')
+  }
+
+  const navigateContact = () => {
+    navigate('/contact')
+  }
+
+  const markSelectedTab = (tabName) => {
+    return location.pathname === tabName ? 'text-primary' : 'cursor-pointer'
+  }
+
   return (
-    <div>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/contact'>Contact</Link>
-        <Link to='/projects'>Projects</Link>
+    <div className="hero mt-20">
+      <div className="hero-content flex-col lg:flex-row gap-10">
+        <img src="https://avatars.githubusercontent.com/u/99290148?v=4" className="h-auto w-40 rounded-full shadow-2xl" />
+        <div>
+          <h1 className="text-4xl font-bold">Adam Gallagher</h1>
+          <ul className='flex gap-4 mt-10'>
+            <li onClick={navigateHome} className={`font-medium text-lg ${markSelectedTab('/')}`}>About</li>
+            <li onClick={navigateProjects} className={`font-medium text-lg ${markSelectedTab('/projects')}`}>Projects</li>
+            <li onClick={navigateContact} className={`font-medium text-lg ${markSelectedTab('/contact')}`}>Contact</li>
+          </ul>
+        </div> 
+      </div>
     </div>
   )
 }
