d6e492c8cb7c9fbb419ea588590eeef20d80e584
Adam <N00211418@iadt.ie>
2023-12-20 15:53:17 +0000

    added contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='hero mt-10'></div>
+    <div className="hero mt-10">
+  <div className="hero-content text-center">
+    <div className="max-w-md">
+      <h1 className="text-5xl font-bold">Lets Connect</h1>
+      <p className="py-6">Let's connect on <a className="link link-primary" href='https://www.linkedin.com/in/adam-gallagher-ba3830263/'>LinkedIn</a> and collaborate on <a className='link link-primary' href='https://github.com/AdamGallagher27'>Github</a>! I believe in the power of networking and collaboration to create meaningful projects and opportunities. Whether you're a fellow developer, designer, or enthusiast in the tech world, I'm always open to connecting with like-minded individuals. Feel free to reach out on LinkedIn to stay updated on my professional journey, and let's explore exciting possibilities by collaborating on GitHub. Together, we can build something amazing!</p>
+    </div>
+  </div>
+</div>
   )
 }
 
